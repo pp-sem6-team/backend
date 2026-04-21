f3da59362bd2f74519da1eccb1656067ea3fd743
kk10 <kamy.zero.10@gmail.com>
2026-04-21 23:15:49 +0300

    [BACKEND-11] refactor(db): rename init migration to up and add down migration

---
+++ migrations/0001_init.down.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS refresh_tokens;
+DROP TABLE IF EXISTS skin_type_ingredients;
+DROP TABLE IF EXISTS ingredients;
+DROP TABLE IF EXISTS recommendations;
+DROP TABLE IF EXISTS analysis;
+DROP TABLE IF EXISTS photos;
+DROP TABLE IF EXISTS users;
+
+DROP TYPE IF EXISTS skin_type;
+DROP TYPE IF EXISTS analysis_status;
+DROP TYPE IF EXISTS gender;
